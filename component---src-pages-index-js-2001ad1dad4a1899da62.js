ba239b93260cf2459e9383c1c53cadf9bf62644a
gatscy <just_do_it@hanmail.net>
2020-12-30 20:19:18 +0900

    Updates

---
+++ component---src-pages-index-js-2001ad1dad4a1899da62.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{RXBc:function(n,t,e){"use strict";e.r(t);var c=e("q1tI"),o=e.n(c);e("q4sD");t.default=function(){return o.a.createElement("div",null,"Hello gatscy!!! 하하하")}},q4sD:function(n,t,e){}}]);
+//# sourceMappingURL=component---src-pages-index-js-2001ad1dad4a1899da62.js.map
